cf7590f4f82c10175af6b3e822652b348eaf5000
Paulo Henrique Prates <t3ctotalmenterandom1@outlook.com>
2020-08-04 18:16:35 -0300

    Add starter bot code

--- Program.cs
+++ Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
-
+using System.Threading.Tasks;
+using sisbase;
 namespace Sokobot_sharp
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var bot = new SisbaseBot();
+            bot.RegisterBot(typeof(Program).Assembly);
+            await bot.Start();
         }
     }
 }
